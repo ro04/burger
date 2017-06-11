b2c59f5fce72a2ae144b0b5631013eab00981e9f
Rochelle Beal <bealrochelle@gmail.com>
2017-06-10 23:19:48 -0500

    add JawsDB

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(__dirname + "/public"));
